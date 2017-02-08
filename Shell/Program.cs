61d72c917cb215509a8b65a0e0ff03f25e164400
rorymurphyza <rory@rorym.co.za>
2017-02-09 05:51:01 +1100

    Add .gitignore for VS

--- Shell/Program.cs
+++ Shell/Program.cs
@@ -11,7 +11,7 @@ namespace Shell
             //Workers.doChallenge3();     //Set 1, Challenge 3 worker and solution
             //Workers.doChallenge4();       //Set 1, Challenge 4 worker and solution
             //Encrypter.RepeatingXOR("Burning 'em, if you ain't quick and nimble I go crazy when I hear a cymbal", "ICE");    //Set 1, Challenge 5
-
+            //This is a change
 
             Console.WriteLine("All done. Press any key to exit.");
             Console.ReadKey();

b74dccdceed24ffd11a432f654bce4af85f786a4
rorymurphyza <rory@rorym.co.za>
2017-02-10 05:18:05 +1100

    Up to PKCS#7 padding for book

--- Shell/Program.cs
+++ Shell/Program.cs
@@ -11,7 +11,9 @@ namespace Shell
             //Workers.doChallenge3();     //Set 1, Challenge 3 worker and solution
             //Workers.doChallenge4();       //Set 1, Challenge 4 worker and solution
             //Encrypter.RepeatingXOR("Burning 'em, if you ain't quick and nimble I go crazy when I hear a cymbal", "ICE");    //Set 1, Challenge 5
-            //This is a change
+            //Workers.BreakRepeatingXOR();    //Set 1, Challenge 6 worker and solution
+            //Workers.AESinECBMode();         //Set 1, Challange 7 worker and solution
+            //Workers.DetectECBEncoding();      //Set 1, Challenge 8 worker and solution
 
             Console.WriteLine("All done. Press any key to exit.");
             Console.ReadKey();

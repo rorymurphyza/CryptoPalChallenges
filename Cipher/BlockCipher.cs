8a2e9176a211a267b30f2512838b062bc98cfbdd
rorymurphyza <rory@rorym.co.za>
2017-02-10 16:39:01 +1100

    Added Cipher interface and start of BlockCipher

---
+++ Cipher/BlockCipher.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cipher
+{
+    class BlockCipher
+    {
+        public class ECBMode : ICipher
+        {
+            public byte[] IV { get; set; }
+            public byte[] key { get; set; }
+            public byte[] cipherText { get; set; }
+            public byte[] plainText { get; set; }
+            public int blockSize { get; set; }
+
+            public byte[] encrypt()
+            {
+                return new byte[1];
+            }
+
+            public byte[] decrypt()
+            {
+                return new byte[1];
+            }
+        }
+    }
+}
---
+++ Cipher/ICipher.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cipher
+{
+    interface ICipher
+    {
+        /// <summary>
+        /// Initial values vector
+        /// </summary>
+        byte[] IV { get; set; }
+        /// <summary>
+        /// The predefined key
+        /// </summary>
+        byte[] key { get; set; }
+        /// <summary>
+        /// The cipher text to be decoded as required
+        /// </summary>
+        byte[] cipherText { get; set; }
+        /// <summary>
+        /// The plain text to be encoded as required
+        /// </summary>
+        byte[] plainText { get; set; }
+        /// <summary>
+        /// The block size of the cipher text, in bytes
+        /// </summary>
+        int blockSize { get; set; }
+
+        byte[] encrypt();
+        byte[] decrypt();
+    }
+}

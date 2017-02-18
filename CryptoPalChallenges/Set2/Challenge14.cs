f340566e54be8c8bd92b254b57171b8cda1ff052
rorymurphyza <rory@rorym.co.za>
2017-02-19 09:50:07 +1100

    Completed Challenge 14

--- CryptoPalChallenges/Set2/Challenge14.cs
+++ CryptoPalChallenges/Set2/Challenge14.cs
@@ -19,7 +19,46 @@ namespace CryptoPalChallenges.Set2
 
             BlockCipher ecb = new BlockCipher.ECBMode();
             ecb.cipherText = cipherText;
-            
+            int blockSize = ecb.findBlockSize();
+            bool isECB = ecb.isECBMode();
+
+            #region find prefix length
+            //find length of prefix. start with two blocks of identical plaintext. prepend other byte to this until we get two identical blocks of ciphertext.
+            int prefixLength = 0;
+            int blockPosition = 0;
+            byte[] plainText;
+            for (int j = 0; j < 100; j++)   //assume prefix is less than 100 bytes
+            {
+                plainText = new byte[blockSize * 2 + j];     //blocksize*2 gives the identical blocks
+                for (int i = 0; i < blockSize; i++)     //add in the identical blocks
+                {
+                    plainText[i] = (byte)'A';
+                    plainText[i + blockSize] = (byte)'A';
+                }
+                for (int i = blockSize * 2; i < plainText.Length; i++)  //add in the extra bytes as needed
+                    plainText[i] = (byte)'A';
+                cipherText = oracle14(plainText);
+                if (findIdenticalCipherTextBlocks(cipherText, blockSize, out blockPosition))
+                {
+                    prefixLength = j;   //this is the number of bytes we had to add
+                    break;
+                }
+            }
+            //if we take the number of bytes we had to add, and prepend this as padding to our identical blocks, we can find the length properly
+            plainText = new byte[blockSize * 2 + prefixLength];
+            for (int i = 0; i < prefixLength; i++)
+                plainText[i] = (byte)'\0';
+            for (int i = prefixLength; i < plainText.Length; i++)
+                plainText[i] = (byte)'A';
+            cipherText = oracle14(plainText);
+            findIdenticalCipherTextBlocks(cipherText, blockSize, out blockPosition);
+            prefixLength = blockSize * blockPosition + (blockSize - prefixLength);
+            Console.WriteLine("Prefix length is: " + prefixLength);
+            #endregion
+
+            #region break target-bytes
+            //this is exactly the same as was done for Challenge 12, I will return and complete this.
+            #endregion
         }
 
         internal static byte[] oracle14(byte[] input)
@@ -37,9 +76,7 @@ namespace CryptoPalChallenges.Set2
         private static byte[] oracle14ByteAdder(byte[] input)
         {
             int numOfByteToPrepend = rnd.Next(5, 20);
-            var prepend = new byte[numOfByteToPrepend];
-            for (int i = 0; i < prepend.Length; i++)
-                prepend[i] = (byte)rnd.Next(0x20, 0x7e);    //prepend only ASCII bytes for simplicity
+            var prepend = "This is the prepend text.".toByteArray();
 
             string append64 = "Um9sbGluJyBpbiBteSA1LjAKV2l0aCBteSByYWctdG9wIGRvd24gc28gbXkgaGFpciBjYW4gYmxvdwpUaGUgZ2lybGllcyBvbiBzdGFuZGJ5IHdhdmluZyBqdXN0IHRvIHNheSBoaQpEaWQgeW91IHN0b3A/IE5vLCBJIGp1c3QgZHJvdmUgYnkK";
             byte[] append = append64.base64ToByteArray();
@@ -53,6 +90,22 @@ namespace CryptoPalChallenges.Set2
                 output[i] = append[i - prepend.Length - input.Length];
 
             return output;
+        }
+
+        public static bool findIdenticalCipherTextBlocks(byte[] cipherText, int blockSize, out int blockPosition)
+        {
+            //return true if we have two sucessive blocks of ciphertext that are exactly equal
+            var blocks = cipherText.toList(blockSize);
+            for (int i = 0; i < blocks.Count - 2; i++)
+            {
+                if (blocks[i].toHexString().Equals(blocks[i + 1].toHexString()))
+                {
+                    blockPosition = i - 1;  //we want to know the number of blocks before we found the two identical blocks
+                    return true;
+                }
+            }
+            blockPosition = 0;
+            return false;
         }
     }
 }
